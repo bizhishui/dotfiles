163ab16dae7a87bc8b379b860acce6271e29af98
Jinming <jmaximelv@gmail.com>
2021-06-23 17:45:13 +0200

    sync

--- ubuntu/miscellaneous/listBkupSerFiles.py
+++ ubuntu/miscellaneous/listBkupSerFiles.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import os,sys 
+from datetime import datetime
 #print (sys.version_info)
 
+saveFreq=10
 if len(sys.argv) < 2:
     print("Need one argument: the ser saved frequency")
-    exit(1)
+    print("The dafault value 10 is used")
+else:
+    saveFreq = int(sys.argv[1])
 
-if os.path.isfile('bkupSerList.txt'):
-    os.remove('bkupSerList.txt')
-of = open('bkupSerList.txt', 'a')
+now = datetime.now()
+date_time = now.strftime("%Y%m%d-%H%M%S")
+#if os.path.isfile('bkupSerList.txt'):
+#    os.remove('bkupSerList.txt')
+of = open('bkupSerList_'+date_time+'.txt', 'a')
 for root,dirs,files in os.walk(os.getcwd()): 
     for unin in ['in', 'wall', 'out']:
         if unin in dirs:
@@ -23,7 +29,7 @@ for root,dirs,files in os.walk(os.getcwd()):
     if len(fl) > 1:
         fl.sort()
         print(os.path.join(root,fl[-1]), file=of)
-        for idx in range(0, len(fl), int(sys.argv[1])):
+        for idx in range(0, len(fl), saveFreq):
             print(os.path.join(root,fl[idx]), file=of)
 
         of.flush()
